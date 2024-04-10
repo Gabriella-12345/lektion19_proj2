1ff6cf8645c9d22fd2d4d5853172b6d8f8e7f93f
gabriella jonsson <gabriella.jonsson@elev.ga.ntig.se>
2024-04-10 10:21:38 +0200

    skapa en form av meny

--- src/App.java
+++ src/App.java
@@ -3,6 +3,24 @@ public class App {
         System.out.println("Hello, World!");
 
         System.out.println("Välkommen till Gabbi Airlines!");
-        System.out.println();
-    }
+        System.out.println("Boka en plats på flyget");
+        System.out.println("Hjälp eller frågor");
+        System.out.println("Vill du ta med ditt husdjur");
+
+        int meny = 1;
+        switch(meny){
+            case 1:
+
+
+            case 2:
+
+
+            case 3:
+
+
+            case 4:
+        
+
+        }
+    }  
 }
