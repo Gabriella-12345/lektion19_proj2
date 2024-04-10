32e60bce19e0e39c145d46bc5da8bd95a63a0d63
gabriella jonsson <gabriella.jonsson@elev.ga.ntig.se>
2024-04-10 10:11:13 +0200

    skapa

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        System.out.println("Välkommen till Gabbi Airlines!");
+        System.out.println();
+    }
+}
